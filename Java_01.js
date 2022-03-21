d7a77761ad2e9700d35baa71073d71a77e36d2ad
SkorodumovaElizaveta <100581712+SkorodumovaElizaveta@users.noreply.github.com>
2022-03-21 17:18:43 +0300

    Add files via upload

---
+++ Java_01.js
@@ -0,0 +1,12 @@
+"Привет JAVA!"
+var myThing;
+myThing=5;
+myThing="Ежи";
+var numberNine=9;
+var stringNine="9";
+numberNine + numberNine;
+stringNine + stringNine;
+var greeting= "Привет, ";
+var myName= "Ёж";
+greeting + myName;
+myName.length;
